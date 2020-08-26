70c82fd54a53f6b2a4c507d3f7830c1e4d48ecc4
Benjamin Vincent <neb_401@me.com>
2020-08-26 03:24:16 -0400

    First Day (#1)
    * fix: Scientia es lux principium✨™
    
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>
    
    * fix: Scientia es lux principium✨™
    
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>
    
    * fix: Scientia es lux principium✨™
    
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>
    
    * partial: add the code and the resource I have used
    
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>
    
    * partial: fix docs
    
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>
    
    * feat: split the functions into separate files
    
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>
    
    * feat: adding implementations of the commands
    
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>
    
    * liscense: MIT
    
    « Scientia es lux principium✨ » IS A TRADEMARK OF
    BENJAMIN VINCENT KASAPOGLU
    <luxcium@neb401.com>
    
     (™ & © 2016-2020 ALL RIGHT RESERVED)
    
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>
    
    * feat: add help section
    
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>
    
    * remove extra information not required
    
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>

--- README.md
+++ README.md
@@ -1,16 +1,20 @@
 
-#  \#100dayofcode
+#  \#100DaysOfcode
 [![Twitter Follow](https://img.shields.io/twitter/follow/Luxcium?label=Follow%20me&style=social)](https://twitter.com/Luxcium?ref_src=github001)
 
+
+Benjamin Vincent✨is doing the #100DaysOfCode challenge, is a Full-time #TypeScript worshiper, #NodeJS enthusiast & #VSCODE junkie. (he/him) cis-gender gay-boy 🇨🇦🍑🤓🐍🦋🍆
+
+ ## My Goals
 
 - Learn every day for 100 day.
 - Code at least one hour per day.
 - Teach someting online.
 - Blog about code every day.
 - Youtube something usefull for other to learn while doing their #100dayof code challenge.
-- Reach to other doing the chalenge.
+- Reach to other doing the challenge.
 - Report my daily experience in a log on my github repository.
-- Support other doing the chalenge
+- Support other doing the challenge
 
 ----
 Scientia es lux principium✨™
---
+++ first-week/day-one/foreachdo.sh
@@ -0,0 +1,55 @@
+function loadforeachdo() {
+  function fordirdo() {
+    for d in *; do
+      [ -d "${d}" ] && eval "${1:-'echo'} ${3}${d}${4} ${2}"
+    done
+  }
+
+  function fordotdirdo() {
+    for d in .*; do
+      [ -d "${d}" ] && eval "${1:-'echo'} ${3}${d}${4} ${2}"
+    done
+  }
+
+  function foreachdirdo() {
+    for d in .* *; do
+      [ -d "${d}" ] && eval "${1:-'echo'} ${3}${d}${4} ${2}"
+    done
+  }
+
+  function foreachfile() {
+    for f in *; do
+      [ -f "${f}" ] && eval "${1:-'echo'} ${3}${f}${4} ${2}"
+    done
+  }
+
+  function foreachline() {
+    while read line; do
+      eval "${1:-'echo'} ${3}${line}${4} ${2}"
+    done
+  }
+}
+loadforeachdo
+
+function unsetforeachdoall() {
+  unsetforeachdo --all
+}
+
+function unsetforeachdo() {
+  if [[ "$1" = "all" || "$1" = "--all" ]]; then
+    unset -f loadforeachdo
+    unset -f unsetforeachdo
+    unset -f unsetforeachdoall
+  fi
+  unset -f fordirdo 2>/dev/null
+  unset -f fordotdirdo 2>/dev/null
+  unset -f foreachdirdo 2>/dev/null
+  unset -f foreachfile 2>/dev/null
+  unset -f foreachline 2>/dev/null
+}
+
+touch fordirdo
+touch fordotdirdo
+touch foreachdirdo
+touch foreachfile
+touch foreachline
