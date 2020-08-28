ed9353f8efdc7452211c47b9fa3493b915182dc3
Benjamin Vincent (Luxcium) <luxcium@neb401.com>
2020-08-28 07:01:42 -0400

    functor must map and fork in my implementation
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>

--- first-week/day-two/functor.ts
+++ first-week/day-two/functor.ts
@@ -1,7 +1,41 @@
+export type FunctorType<T = any> = IFMap<T> & IFork<T>;
+export type ForkType<A = any> = A;
+export type MapType<A = any> /* <A = any, U = any> = <B = U> */ = (
+  fn: (val: A) => any,
+) => IFMap;
+export interface IFMap<A = any> /* <A = any, U = any> extends ValueType<A> */ {
+  /** fantasy-land/map :: Functor f => f a ~> (a -> b) -> f b */
+  map: MapType<A> /* <A, U> */;
+}
+
+/** A functor must, in our implementation, Fork and return its internat value */
+export interface IFork<A = any> extends ValueType<A> {
+  readonly fork: ForkType<A>;
+}
+export interface ValueType<A = any> {
+  readonly _value: A;
+}
+
+/** A functor must FMap and in our implementation of a functor must Fork */
+
+
+export interface IFunctor<A = any> extends IFMap<A>, IFork<A>, FunctorType<A> {
+  map<B>(fn: (val: A) => B): FunctorType<B>;
+  toString(): string;
+  toValue(): ValueType<A>;
+  readonly fork: A;
+}
+
+
 export class Functor<T = unknown>  {
   public _value: T;
 
   public constructor(value: T) {
     this._value = value;
   }
+
+    public map<R = any>(fn: (val: T) => R): IFunctor<R> {
+    return new Functor<R>(fn(this._value));
+  }
+
 }
