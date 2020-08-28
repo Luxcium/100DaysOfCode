54ef9193a878f157a51aee1a9612a90fff99169c
Benjamin Vincent <neb_401@me.com>
2020-08-28 08:32:56 -0400

    Second Day (#2)
    * feat: tag list ordinal english number names 0-100
    
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>
    
    * wakatime tracker
    
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>
    
    * catching up: working on day two
    
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>
    
    * catching up: working on day two
    
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>
    
    * catching up: working on day one
    
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>
    
    * from recent project will refactor in typed chunks
    
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>
    
    * LUXCIUM LICENSE PROVIDED "AS IS" WITHOUT WARRANTY
    
    *NO* PERMISSION GRANTED
    
    ================================================================================≈
    † LUXCIUM‡ LICENSE — *NO* PERMISSION GRANTED — PROVIDED "AS IS" WITHOUT WARRANTY
    
    ‽  *NO* PERMISSION ARE GRANTED. NOT TO PUBLISH, NOT TO DISTRIBUTE, NOT TO
    ‽  SUBLICENSE, AND/OR NOT TO SELL COPIES OF THE SOFTWARE.
    ‽
    ‽  You may use, only for yourself, the source code in this file and from
    ‽  imported files to this one. You have the right to: distribute to your
    ‽  friends, to your students, or to you coworker for personal or academic use
    ‽  at home at school or at work. You must include this «FULL NOTICE» in each
    ‽  imported files to this one that you decide to use if such «FULL NOTICE»
    ‽  is missing. If you modify the code in a substatial way you may decide to
    ‽  include only the «COPYRIGHT» and the «SHORT NOTICE» below.
    
    † Copyright © 2020 — LUXCIUM‡ (Benjamin Vincent Kasapoglu) <luxcium⸓neb401.com>
    
    - --> SHORT NOTICE
    -  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ALL KIND, EXPRESS OR
    -  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
    -  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
    -  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ALL CLAIM, DAMAGES OR OTHER
    -  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
    -  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
    -  SOFTWARE.
    - -->
    -
    - IN ANY CASES THE COPYRIGHT AND SHORT NOTICE ABOVE MUST BE INCLUDED.
    
    † Scientia es lux principium✨ ™
    ================================================================================≈
    
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>
    
    * add: Functor<T> type
    
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>
    
    * functor must map and fork in my implementation
    
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>
    
    * moving out code to start again fresh
    
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>
    
    * A simple Functor must map
    
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>
    
    * error: Type 'unknown' is not assignable to type 'T'.
    
    'T' could be instantiated with an arbitrary type which could be unrelated to
    'unknown'.ts(2322)
    
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>
    
    * use return this._value as T as a workaround
    
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>
    
    * coding for day two completed will revisit later
    
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>

---
+++ first-week/day-two/functor.ts
@@ -0,0 +1,50 @@
+/** A Map takes any value A and morph it into any value B */
+export type MapType<A = unknown> = (
+  fn: <B = unknown>(val: A) => B,
+) => IFMap;
+
+/**
+ * Anything that implement a Map must implement
+ * a method of the form map: MapType<A>
+ */
+export interface IFMap<A = unknown> {
+  /** fantasy-land/map :: Functor f => f a ~> (a -> b) -> f b */
+  map: MapType<A>;
+}
+
+/** A simple Functor must map */
+export class FunctorSimplex<T = unknown> implements IFMap<T> {
+  protected _value: T;
+  public constructor(value: T) {
+    this._value = value;
+  }
+  public map<R = unknown>(fn: (val: T) => R) {
+    return new FunctorSimplex<R>(fn(this._value));
+  }
+}
+
+export function testFunctorSimplex() {
+  const myFunctorOne = new FunctorSimplex('10')
+  myFunctorOne.map((a: string) => a)
+  myFunctorOne.map((a: string): number => a.length)
+}
+
+
+// export type ForkType<A = unknown> = A;
+export interface IFork<A = unknown> { // extends ValueType<A>
+  readonly fork: A;
+}
+// export interface ValueType<A = any> {
+//   readonly _value: A;
+// }
+
+/** A complex Functor must extend simple Functor and fork */
+export class FunctorComplex<T = unknown> extends FunctorSimplex implements IFork<T> {
+  public constructor(value: T) {
+    super(value)
+  }
+  public get fork(): T {
+    return this._value as T
+  }
+
+}
