36b264020c53e52b0920ac8bbfa14536d0dd4c84
Benjamin Vincent (Luxcium) <luxcium@neb401.com>
2020-08-28 07:47:05 -0400

    use return this._value as T as a workaround
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>

--- first-week/day-two/functor.ts
+++ first-week/day-two/functor.ts
@@ -1,19 +1,19 @@
 /** A Map takes any value A and morph it into any value B */
-export type MapType<A = unknown>  = (
-  fn: <B=unknown>(val: A) => B,
+export type MapType<A = unknown> = (
+  fn: <B = unknown>(val: A) => B,
 ) => IFMap;
 
 /**
  * Anything that implement a Map must implement
  * a method of the form map: MapType<A>
  */
-export interface IFMap<A = unknown>  {
+export interface IFMap<A = unknown> {
   /** fantasy-land/map :: Functor f => f a ~> (a -> b) -> f b */
   map: MapType<A>;
 }
 
 /** A simple Functor must map */
-export class FunctorSimplex<T = unknown>  {
+export class FunctorSimplex<T = unknown> implements IFMap<T> {
   protected _value: T;
   public constructor(value: T) {
     this._value = value;
@@ -25,14 +25,14 @@ export class FunctorSimplex<T = unknown>  {
 
 export function testFunctorSimplex() {
   const myFunctorOne = new FunctorSimplex('10')
-  myFunctorOne.map((a:string)=>a)
-  myFunctorOne.map((a:string):number=>a.length)
+  myFunctorOne.map((a: string) => a)
+  myFunctorOne.map((a: string): number => a.length)
 }
 
 
-export type ForkType<A = unknown> = A;
-export interface IFork<A = unknown>  { // extends ValueType<A>
-  readonly fork: ForkType<A>;
+// export type ForkType<A = unknown> = A;
+export interface IFork<A = unknown> { // extends ValueType<A>
+  readonly fork: A;
 }
 // export interface ValueType<A = any> {
 //   readonly _value: A;
@@ -41,10 +41,10 @@ export interface IFork<A = unknown>  { // extends ValueType<A>
 /** A complex Functor must extend simple Functor and fork */
 export class FunctorComplex<T = unknown> extends FunctorSimplex implements IFork<T> {
   public constructor(value: T) {
-   super(value)
+    super(value)
   }
-  public get fork():T {
-  return this._value
-}
+  public get fork(): T {
+    return this._value as T
+  }
 
 }
