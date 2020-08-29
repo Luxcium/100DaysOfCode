03f42939fd8d2ae50e71c91507ec3ae4f31fdaf7
Benjamin Vincent (Luxcium) <luxcium@neb401.com>
2020-08-29 06:40:37 -0400

    FunctorComplex extend FunctorSimplex and must fork
    Tanks to Basarat Ali Syed @basarat for helping me on this
    one as reflected in #3
    
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>

--- first-week/day-two/functor.ts
+++ first-week/day-two/functor.ts
@@ -14,12 +14,24 @@ export interface IFMap<A = unknown> {
 
 /** A simple Functor must map */
 export class FunctorSimplex<T = unknown> implements IFMap<T> {
-  protected _value: T;
+  protected value!: T;
   public constructor(value: T) {
-    this._value = value;
+
+    const functorSimplex = {
+      value: {
+        value,
+        configurable: false,
+        enumerable: true,
+        writable: false,
+      },
+
+    }
+    // this._value = value;
+
+    Object.defineProperties(this, functorSimplex);
   }
-  public map<R = unknown>(fn: (val: T) => R) {
-    return new FunctorSimplex<R>(fn(this._value));
+  public map<R = unknown>(fn: (val: T) => R): FunctorSimplex<R> {
+    return new FunctorSimplex<R>(fn(this.value));
   }
 }
 
@@ -32,6 +44,7 @@ export function testFunctorSimplex() {
 
 // export type ForkType<A = unknown> = A;
 export interface IFork<A = unknown> { // extends ValueType<A>
+  /** Return the internal value of a Functor or type extending Functor */
   readonly fork: A;
 }
 // export interface ValueType<A = any> {
@@ -39,12 +52,37 @@ export interface IFork<A = unknown> { // extends ValueType<A>
 // }
 
 /** A complex Functor must extend simple Functor and fork */
-export class FunctorComplex<T = unknown> extends FunctorSimplex implements IFork<T> {
+export class FunctorComplex<T = unknown> extends FunctorSimplex<T> implements IFMap<T>, IFork<T> {
   public constructor(value: T) {
     super(value)
+    return this
   }
-  public get fork(): T {
-    return this._value as T
+  public map<R = unknown>(fn: (val: T) => R): FunctorComplex<R> {
+    return new FunctorComplex<R>(fn(this.fork));
   }
 
+  /** Return the internal value of a Functor or type extending Functor */
+  public get fork(): T {
+    return this.value;
+  }
+
+  /** Returns a string representation of a Functor or type extending Functor. */
+  public toString(): string {
+    return JSON.stringify(this.fork);
+  }
+  /** Returns a value representation of a Functor or type extending Functor. */
+  public toValue(): T {
+    return JSON.parse(this.toString());
+  }
 }
+
+
+
+export function testFunctorComplex() {
+  const myFunctorTwo = new FunctorComplex('10')
+  myFunctorTwo.map((a: string) => a)
+  const aFunctorSmplx: FunctorSimplex<number> = myFunctorTwo.map((a: string): number => a.length);
+  // const aFunctorCmplx: FunctorComplex<number> = aFunctorSmplx.map((b: number) => b * 2)
+  console.log(aFunctorSmplx)
+}
+testFunctorComplex()
