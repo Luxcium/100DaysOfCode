f2849b221ef68d5c705df07e506028f4eef9618c
Benjamin Vincent <neb_401@me.com>
2020-08-29 08:58:07 -0400

    day three (#101)
    * feat: tag list ordinal english number names 0-100
    
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>
    
    * wakatime tracker
    
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>
    
    * catching up: working on day two
    
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>
    
    * catching up: working on day three
    
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>
    
    * FunctorComplex extend FunctorSimplex and must fork
    
    Tanks to Basarat Ali Syed @basarat for helping me on this
    one as reflected in #3
    
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>
    
    * †LUXCIUM LICENSE PROVIDED "AS IS"—WITHOUT WARRANTY
    
    /*
    ================================================================================≈
    † LUXCIUM‡ LICENSE — *NO* PERMISSION GRANTED — PROVIDED "AS IS" WITHOUT WARRANTY
    
    ‽  *NO* PERMISSION ARE GRANTED. NOT TO PUBLISH, NOT TO DISTRIBUTE, NOT TO
    ‽  SUBLICENSE, AND/OR NOT TO SELL COPIES OF THE SOFTWARE.
    ‽
    ‽  You may use, only for yourself, the source code in this file and from
    ‽  imported files to this one. You have the right to: distribute to your
    ‽  friends, to your students, or to you coworker for personal or academic use
    ‽  at home at school or at work. You must include this «FULL NOTICE» in each
    ‽  imported files to this one that you decide to use if such «FULL NOTICE»
    ‽  is missing. If you modify the code in a substatial way you may decide to
    ‽  include only the «COPYRIGHT» and the «SHORT NOTICE» below.
    
    † Copyright © 2020 — LUXCIUM‡ (Benjamin Vincent Kasapoglu) <luxcium⸓neb401.com>
    
    - --> SHORT NOTICE
    -  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ALL KIND, EXPRESS OR
    -  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
    -  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
    -  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ALL CLAIM, DAMAGES OR OTHER
    -  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
    -  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
    -  SOFTWARE.
    - -->
    -
    - IN ANY CASES THE COPYRIGHT AND SHORT NOTICE ABOVE MUST BE INCLUDED.
    
    † Scientia es lux principium✨ ™
    ================================================================================≈
    */
    
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>

--- first-week/day-three/monad.ts
+++ first-week/day-three/monad.ts
@@ -1,3 +1,4 @@
+
 /**
  * LUXCIUM LICENSE *NO* PERMISSION GRANTED - PROVIDED "AS IS" - WITHOUT WARRANTY
  * =============================================================================≈
@@ -6,52 +7,7 @@
  * @license LUXCIUM-LICENSE for this file only or other having the same mention
  * =============================================================================≈
  * Scientia es lux principium✨ ™ - SEE THE BOTTOM OF THIS FILES FOR MORE INFO
-*/
-
-export type FunctorType<T = any> = IFMap<T> & IFork<T>;
-export type ForkType<A = any> = A;
-export type MapType<A = any> /* <A = any, U = any> = <B = U> */ = (
-  fn: (val: A) => any,
-) => IFMap;
-export interface IFMap<A = any> /* <A = any, U = any> extends ValueType<A> */ {
-  /** fantasy-land/map :: Functor f => f a ~> (a -> b) -> f b */
-  map: MapType<A> /* <A, U> */;
-}
-
-
-
-/** A functor must, in our implementation, Fork and return its internat value */
-export interface IFork<A = any> extends ValueType<A> {
-  readonly fork: ForkType<A>;
-}
-export interface ValueType<A = any> {
-  readonly _value: A;
-}
-
-/** A functor must FMap and in our implementation of a functor must Fork */
-
-
-export interface IFunctor<A = any> extends IFMap<A>, IFork<A>, FunctorType<A> {
-  map<B>(fn: (val: A) => B): FunctorType<B>;
-  toString(): string;
-  toValue(): ValueType<A>;
-  readonly fork: A;
-}
-
-
-// export class Functor<T = unknown>  {
-//   public _value: T;
-
-//   public constructor(value: T) {
-//     this._value = value;
-//   }
-
-//     public map<R = any>(fn: (val: T) => R): IFunctor<R> {
-//     return new Functor<R>(fn(this._value));
-//   }
-
-// }
-
+ */
 
 /*
 ================================================================================≈
--- first-week/day-two/functor.ts
+++ first-week/day-two/functor.ts
@@ -1,3 +1,12 @@
+/**
+ * LUXCIUM LICENSE *NO* PERMISSION GRANTED - PROVIDED "AS IS" - WITHOUT WARRANTY
+ * =============================================================================≈
+ * @author Benjamin Vincent Kasapoglu (LUXCIUM)
+ * @copyright © 2020 - LUXCIUM (Benjamin Vincent Kasapoglu) <luxcium@neb401.com>
+ * @license LUXCIUM-LICENSE for this file only or other having the same mention
+ * =============================================================================≈
+ * Scientia es lux principium✨ ™ - SEE THE BOTTOM OF THIS FILES FOR MORE INFO
+ */
 /** A Map takes any value A and morph it into any value B */
 export type MapType<A = unknown> = (
   fn: <B = unknown>(val: A) => B,
@@ -14,12 +23,24 @@ export interface IFMap<A = unknown> {
 
 /** A simple Functor must map */
 export class FunctorSimplex<T = unknown> implements IFMap<T> {
-  protected _value: T;
+  protected value!: T;
   public constructor(value: T) {
-    this._value = value;
+
+    const functorSimplex = {
+      value: {
+        value,
+        configurable: false,
+        enumerable: true,
+        writable: false,
+      },
+
+    }
+    // this._value = value;
+
+    Object.defineProperties(this, functorSimplex);
   }
-  public map<R = unknown>(fn: (val: T) => R) {
-    return new FunctorSimplex<R>(fn(this._value));
+  public map<R = unknown>(fn: (val: T) => R): FunctorSimplex<R> {
+    return new FunctorSimplex<R>(fn(this.value));
   }
 }
 
@@ -32,6 +53,7 @@ export function testFunctorSimplex() {
 
 // export type ForkType<A = unknown> = A;
 export interface IFork<A = unknown> { // extends ValueType<A>
+  /** Return the internal value of a Functor or type extending Functor */
   readonly fork: A;
 }
 // export interface ValueType<A = any> {
@@ -39,12 +61,75 @@ export interface IFork<A = unknown> { // extends ValueType<A>
 // }
 
 /** A complex Functor must extend simple Functor and fork */
-export class FunctorComplex<T = unknown> extends FunctorSimplex<T> implements IFork<T> {
+
+export class FunctorComplex<T = unknown> extends FunctorSimplex<T> implements IFMap<T>, IFork<T> {
+
   public constructor(value: T) {
     super(value)
+    return this
   }
-  public get fork(): T {
-    return this._value
+  public map<R = unknown>(fn: (val: T) => R): FunctorComplex<R> {
+    return new FunctorComplex<R>(fn(this.fork));
   }
 
+  /** Return the internal value of a Functor or type extending Functor */
+  public get fork(): T {
+
+    return this.value;
+
+  }
+
+  /** Returns a string representation of a Functor or type extending Functor. */
+  public toString(): string {
+    return JSON.stringify(this.fork);
+  }
+  /** Returns a value representation of a Functor or type extending Functor. */
+  public toValue(): T {
+    return JSON.parse(this.toString());
+  }
 }
+
+
+
+export function testFunctorComplex() {
+  const myFunctorTwo = new FunctorComplex('10')
+  myFunctorTwo.map((a: string) => a)
+  const aFunctorSmplx: FunctorSimplex<number> = myFunctorTwo.map((a: string): number => a.length);
+  // const aFunctorCmplx: FunctorComplex<number> = aFunctorSmplx.map((b: number) => b * 2)
+  console.log(aFunctorSmplx)
+}
+testFunctorComplex()
+
+
+/*
+================================================================================≈
+† LUXCIUM‡ LICENSE — *NO* PERMISSION GRANTED — PROVIDED "AS IS" WITHOUT WARRANTY
+
+‽  *NO* PERMISSION ARE GRANTED. NOT TO PUBLISH, NOT TO DISTRIBUTE, NOT TO
+‽  SUBLICENSE, AND/OR NOT TO SELL COPIES OF THE SOFTWARE.
+‽
+‽  You may use, only for yourself, the source code in this file and from
+‽  imported files to this one. You have the right to: distribute to your
+‽  friends, to your students, or to you coworker for personal or academic use
+‽  at home at school or at work. You must include this «FULL NOTICE» in each
+‽  imported files to this one that you decide to use if such «FULL NOTICE»
+‽  is missing. If you modify the code in a substatial way you may decide to
+‽  include only the «COPYRIGHT» and the «SHORT NOTICE» below.
+
+† Copyright © 2020 — LUXCIUM‡ (Benjamin Vincent Kasapoglu) <luxcium⸓neb401.com>
+
+- --> SHORT NOTICE
+-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ALL KIND, EXPRESS OR
+-  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+-  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ALL CLAIM, DAMAGES OR OTHER
+-  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+-  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+-  SOFTWARE.
+- -->
+-
+- IN ANY CASES THE COPYRIGHT AND SHORT NOTICE ABOVE MUST BE INCLUDED.
+
+† Scientia es lux principium✨ ™
+================================================================================≈
+*/
