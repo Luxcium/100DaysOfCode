d89aba98529edcaa9b27c484a668fc181eace6cf
Benjamin Vincent (Luxcium) <luxcium@neb401.com>
2020-09-16 21:10:33 -0400

    remove tslint suppoort for lodash and lodash/fp
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>

--- packages/apply/src/apply.ts
+++ packages/apply/src/apply.ts
@@ -1,10 +1,52 @@
-import { FunctorComplex, FunctorSimplex } from 'functor';
+import { FunctorComplex, FunctorSimplex, IFMap, IFork } from 'functor';
 // fantasy-land/ap :: Apply f => f a ~> f (a -> b) -> f b
 export const FANTASY_LAND_APPLY = 'fantasy-land/ap';
-export class Apply extends FunctorComplex {
-  public ap: any;
-}
+// export class Apply extends FunctorComplex {
+//   public ap: any;
+// }
 export { FunctorSimplex, FunctorComplex };
+
+export class Apply<T = unknown>
+  extends FunctorComplex<T>
+  implements IFMap<T>, IFork<T>, FunctorSimplex<T>, FunctorComplex<T> {
+  public constructor(value: T) {
+    super(value);
+    const functorComplex = {
+      'fantasy-land/map': {
+        configurable: false,
+        enumerable: false,
+        value: this.map,
+        writable: true,
+      },
+    };
+    return Object.defineProperties(this, functorComplex);
+  }
+
+  public 'fantasy-land/map' = this.map;
+
+  public map<R = unknown>(fn: (val: T) => R): FunctorComplex<R> {
+    if (typeof fn === 'function') {
+      return new FunctorComplex<R>(fn(this.fork));
+    }
+    throw new Error(
+      'If argument is not a function, the behaviour of map is unspecified',
+    );
+  }
+
+  public get fork(): T {
+    return this.value;
+  }
+
+  /** Returns a string representation of a Functor or a Functor extended type. */
+  public toString(): string {
+    return JSON.stringify(this.fork);
+  }
+
+  /** Returns a value `T` 'representation' of a Functor or a Functor extended type. */
+  public toValue(): T {
+    return JSON.parse(this.toString());
+  }
+}
 
 /*
 Apply
