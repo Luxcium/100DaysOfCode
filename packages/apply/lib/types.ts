b05cff87e7eb55faf9920248f3d912dcc61e8814
Benjamin Vincent (Luxcium) <luxcium@neb401.com>
2020-09-16 22:44:05 -0400

    Question: how can I have a Generic Generic 'Apply' 23 out of #100DaysOfCode I have a problem related to @TypeScript #GenericTypes implementing #FantasyLand #JS 'b must be same Apply as a'
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>

--- packages/apply/lib/types.ts
+++ packages/apply/lib/types.ts
@@ -1,29 +1,24 @@
 /* eslint-disable no-use-before-define */
-// must apply the function in Apply b to the value in Apply a
-
 import { FunctorComplex } from 'functor';
 import { Apply } from '../src';
 
-// import { Apply } from '../src/apply';
-
-/** An Apply `a.ap(b)` must apply the function in Apply b to the value in Apply a */
+/**
+ * 1. An Apply `a.ap(b)` must apply the function in
+ * Apply `b` to the value in Apply `a`
+ * 2. `b` must be same Apply as `a`
+ * 3. The Apply returned must be the same as a and b
+ */
 export type ApType<A = unknown> = <B = unknown>(
   ap: Apply<(val: A) => B>,
 ) => IApply<B>;
+
 /**
  * Anything that implement an Ap must implement
  * a method of the form map: ap: ApType<A>
  */
-
 export interface IApply<A = unknown> extends FunctorComplex<A> {
   /** `Fantasy-land/ap :: Apply f => f a ~> f (a -> b) -> f b` */
   // 'fantasy-land/ap': ApType<A>;
   /** `ap :: Apply f => f a ~> f (a -> b) -> f b` */
   ap: ApType<A>;
 }
-
-//  IApply<T>,
-//     IFork<T>,
-//     FunctorComplex<T>,
-//     IFMap<T>,
-//     FunctorSimplex<T>
