6c896cd25b4058e2e66c8f6e6969e6d41132b91d
Benjamin Vincent (Luxcium) <luxcium@neb401.com>
2020-09-23 01:36:38 -0400

    color theme mappings
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>

---
+++ third-week/fifteenth-day/ColorMapping-work-file2.js
@@ -0,0 +1,524 @@
+const dark01 = ['activityBar.dropBackground', // (1f)
+'activityBar.foreground', //
+'activityBar.inactiveForeground', // (99)
+'activityBarBadge.foreground', //
+'badge.foreground', //
+'button.foreground', //
+'editor.rangeHighlightBackground', // (0b)
+'extensionBadge.remoteForeground', //
+'extensionButton.prominentForeground', //
+'list.activeSelectionForeground', //
+'menu.selectionForeground', //
+'menubar.selectionBackground', // (1a)
+'panel.dropBackground', // (1f)
+'peekViewResult.fileForeground', //
+'peekViewResult.selectionForeground', //
+'peekViewTitleLabel.foreground', //
+'sideBar.dropBackground', // (1f)
+'statusBar.debuggingForeground', //
+'statusBar.foreground', //
+'statusBar.noFolderForeground', //
+'statusBarItem.activeBackground', // (2e)
+'statusBarItem.hoverBackground', // (1f)
+'statusBarItem.prominentForeground', //
+'statusBarItem.remoteForeground', //
+'tab.activeForeground', //
+'tab.inactiveForeground', // (80)
+'tab.unfocusedActiveForeground', // (80)
+'tab.unfocusedInactiveForeground', // (40)
+'terminal.selectionBackground', // (40)
+'textSeparator.foreground', // (2e)
+]
+const dark02 = [
+
+'checkbox.foreground', //
+'dropdown.foreground', //
+'settings.checkboxForeground', //
+  'settings.dropdownForeground', //
+
+];const   dark03 = [
+'panelTitle.activeForeground', //
+'panelTitle.inactiveForeground', // (99)
+  'settings.headerForeground', //
+
+];const   dark04 = [
+'terminal.ansiBrightWhite', //
+  'terminal.ansiWhite', //
+
+];const   dark05 = [
+
+'editor.foreground', //
+'editorSuggestWidget.foreground', //
+  'foreground', //
+];const   dark06 = [
+'breadcrumb.foreground', // (cc)
+'descriptionForeground', // (b3)
+'editorWidget.foreground', //
+'foreground', //
+'input.foreground', //
+'menu.foreground', //
+'menubar.selectionForeground', //
+'notifications.foreground', //
+'peekViewTitleDescription.foreground', // (b3)
+'settings.numberInputForeground', //
+'settings.textInputForeground', //
+'terminal.foreground', //
+'titleBar.activeForeground', //
+  'titleBar.inactiveForeground', // (99)
+
+];const   dark07 = [
+'editorActiveLineNumber.foreground', //
+  'editorLineNumber.activeForeground', //
+
+];const   dark08 = [
+'menu.separatorBackground', //
+'peekViewResult.lineForeground', //
+  'sideBarTitle.foreground', //
+
+];const   dark09 = [
+'editorOverviewRuler.bracketMatchForeground', //
+'editorOverviewRuler.selectionHighlightForeground', // (cc)
+  'editorOverviewRuler.wordHighlightForeground', // (cc)
+
+];const   dark10 = [
+'imagePreview.border', // (59)
+'panel.border', // (59)
+'panelTitle.activeBorder', // (59)
+'sideBarSectionHeader.background', // (33)
+  'terminal.border', // (59)
+
+];const   dark11 = [
+'editorOverviewRuler.border', // (4d)
+  'textBlockQuote.background', // (1a)
+
+];const   dark12 = [
+'editorOverviewRuler.commonContentForeground', // (66)
+'merge.commonContentBackground', // (29)
+  'merge.commonHeaderBackground', // (66)
+
+];const   dark13 = [
+'editorHoverWidget.border', //
+'editorSuggestWidget.border', //
+'editorWidget.border', //
+'settings.dropdownListBorder', //
+  'checkbox.background', //
+
+];const   dark14 = [
+'checkbox.border', //
+'dropdown.background', //
+'dropdown.border', //
+'input.background', //
+'settings.checkboxBackground', //
+'settings.checkboxBorder', //
+'settings.dropdownBackground', //
+'settings.dropdownBorder', //
+'titleBar.activeBackground', //
+  'titleBar.inactiveBackground', // (99)
+
+];const   dark15 = [
+'editor.findRangeHighlightBackground', // (66)
+  'editor.inactiveSelectionBackground', //
+
+];const   dark16 = [
+'activityBar.background', //
+  'debugToolBar.background', //
+
+];const   dark17 = [
+'notificationCenterHeader.background', //
+  'notifications.border', //
+
+];const   dark18 = [
+'settings.numberInputBackground', //
+  'settings.textInputBackground', //
+
+];const   dark19 = [
+'breadcrumbPicker.background', //
+'editorGroupHeader.tabsBackground', //
+'editorHoverWidget.background', //
+'editorSuggestWidget.background', //
+'editorWidget.background', //
+'menu.background', //
+'notifications.background', //
+'peekViewResult.background', //
+'quickInput.background', //
+'sideBar.background', //
+  'tab.border', //
+
+];const   dark20 = [
+'breadcrumb.background', //
+'editor.background', //
+'editorGroupHeader.noTabsBackground', //
+'editorGutter.background', //
+'editorPane.background', //
+'panel.background', //
+'peekViewTitle.background', //
+'tab.activeBackground', //
+'tab.unfocusedActiveBackground', //
+  'terminal.background', //
+
+];const   dark21 = [
+'editorUnnecessaryCode.opacity', // (aa)
+'listFilterWidget.outline', // (00)
+'scrollbar.shadow', //
+'statusBarItem.prominentBackground', // (80)
+'statusBarItem.prominentHoverBackground', // (4d)
+'terminal.ansiBlack', //
+  'widget.shadow', //
+]
+
+'editorInfo.foreground', //
+'editorMarkerNavigationInfo.background', //
+'editorOverviewRuler.infoForeground', //
+'editorOverviewRuler.incomingContentForeground', // (80)
+'merge.incomingContentBackground', // (33)
+'merge.incomingHeaderBackground', // (80)
+'notificationLink.foreground', //
+'pickerGroup.foreground', //
+'textLink.activeForeground', //
+'textLink.foreground', //
+'editor.hoverHighlightBackground', // (40)
+'editor.selectionBackground', //
+'tab.activeModifiedBorder', //
+'tab.inactiveModifiedBorder', // (80)
+'tab.unfocusedActiveModifiedBorder', // (80)
+'tab.unfocusedInactiveModifiedBorder', // (40)
+'editorSuggestWidget.highlightForeground', //
+'list.highlightForeground', //
+'activityBarBadge.background', //
+'editorOverviewRuler.addedForeground', // (99)
+'editorOverviewRuler.deletedForeground', // (99)
+'editorOverviewRuler.modifiedForeground', // (99)
+'editorOverviewRuler.rangeHighlightForeground', // (99)
+'extensionBadge.remoteBackground', //
+'inputOption.activeBorder', // (00)
+'inputValidation.infoBorder', //
+'peekView.border', //
+'statusBar.background', //
+'textBlockQuote.border', // (80)
+'button.background', //
+'list.activeSelectionBackground', //
+'menu.selectionBackground', //
+'editorSuggestWidget.selectedBackground', //
+'list.focusBackground', //
+'peekViewEditor.background', //
+'peekViewEditorGutter.background', //
+'editorInfo.foreground', //
+'editorLightBulbAutoFix.foreground', //
+'editorMarkerNavigationInfo.background', //
+'editorOverviewRuler.infoForeground', //
+'notificationsInfoIcon.foreground', //
+'editorOverviewRuler.incomingContentForeground', // (80)
+'merge.incomingContentBackground', // (33)
+'merge.incomingHeaderBackground', // (80)
+'notificationLink.foreground', //
+'pickerGroup.foreground', //
+'textLink.activeForeground', //
+'textLink.foreground', //
+'editor.hoverHighlightBackground', // (40)
+'editor.selectionBackground', //
+'minimap.selectionHighlight', //
+'gitDecoration.addedResourceForeground', //
+'editor.focusedStackFrameHighlightBackground', //
+'gitDecoration.untrackedResourceForeground', //
+'editorLink.activeForeground', //
+'editorOverviewRuler.incomingContentForeground', // (80)
+'merge.incomingContentBackground', // (33)
+'merge.incomingHeaderBackground', // (80)
+'terminal.ansiBrightGreen', //
+'statusBarItem.remoteBackground', //
+'editorGutter.addedBackground', //
+'extensionButton.prominentBackground', //
+'extensionButton.prominentHoverBackground', //
+'editorBracketMatch.background', //
+'editorOverviewRuler.wordHighlightStrongForeground', //
+'gitDecoration.conflictingResourceForeground', //
+'terminal.ansiMagenta', //
+'terminal.ansiBrightMagenta', //
+'statusBar.noFolderBackground', //
+'gitDecoration.modifiedResourceForeground', //
+'textPreformat.foreground', //
+'editorMarkerNavigationWarning.background', //
+'editorOverviewRuler.warningForeground', //
+'editorWarning.foreground', //
+'list.warningForeground', //
+'inputValidation.warningBorder', //
+'list.invalidItemForeground', //
+'inputValidation.warningBackground', //
+'terminal.ansiYellow', //
+'terminal.ansiBrightYellow', //
+'editor.stackFrameHighlightBackground', //
+'statusBar.debuggingBackground', //
+'editorOverviewRuler.findMatchForeground', // (7e)
+'minimap.findMatchHighlight', //
+'peekViewEditor.matchHighlightBackground', //
+'editor.findMatchHighlightBackground', // (55)
+'peekViewResult.matchHighlightBackground', // (4d)
+
+
+/*
+
+
+|Dark 01|activityBar.dropBackground (1f)
+|Dark 01|activityBar.foreground
+|Dark 01|activityBar.inactiveForeground (99)
+|Dark 01|activityBarBadge.foreground
+|Dark 01|badge.foreground
+|Dark 01|button.foreground
+|Dark 01|editor.rangeHighlightBackground (0b)
+|Dark 01|extensionBadge.remoteForeground
+|Dark 01|extensionButton.prominentForeground
+|Dark 01|list.activeSelectionForeground
+|Dark 01|menu.selectionForeground
+|Dark 01|menubar.selectionBackground (1a)
+|Dark 01|panel.dropBackground (1f)
+|Dark 01|peekViewResult.fileForeground
+|Dark 01|peekViewResult.selectionForeground
+|Dark 01|peekViewTitleLabel.foreground
+|Dark 01|sideBar.dropBackground (1f)
+|Dark 01|statusBar.debuggingForeground
+|Dark 01|statusBar.foreground
+|Dark 01|statusBar.noFolderForeground
+|Dark 01|statusBarItem.activeBackground (2e)
+|Dark 01|statusBarItem.hoverBackground (1f)
+|Dark 01|statusBarItem.prominentForeground
+|Dark 01|statusBarItem.remoteForeground
+|Dark 01|tab.activeForeground
+|Dark 01|tab.inactiveForeground (80)
+|Dark 01|tab.unfocusedActiveForeground (80)
+|Dark 01|tab.unfocusedInactiveForeground (40)
+|Dark 01|terminal.selectionBackground (40)
+|Dark 01|textSeparator.foreground (2e)
+
+|Dark 02|checkbox.foreground
+|Dark 02|dropdown.foreground
+|Dark 02|settings.checkboxForeground
+|Dark 02|settings.dropdownForeground
+
+|Dark 03|panelTitle.activeForeground
+|Dark 03|panelTitle.inactiveForeground (99)
+|Dark 03|settings.headerForeground
+
+|Dark 04|terminal.ansiBrightWhite
+|Dark 04|terminal.ansiWhite
+
+|Dark 05|editor.foreground
+|Dark 05|editorSuggestWidget.foreground
+|Dark 05|foreground
+
+|Dark 06|breadcrumb.foreground (cc)
+|Dark 06|descriptionForeground (b3)
+|Dark 06|editorWidget.foreground
+|Dark 06|foreground
+|Dark 06|input.foreground
+|Dark 06|menu.foreground
+|Dark 06|menubar.selectionForeground
+|Dark 06|notifications.foreground
+|Dark 06|peekViewTitleDescription.foreground (b3)
+|Dark 06|settings.numberInputForeground
+|Dark 06|settings.textInputForeground
+|Dark 06|terminal.foreground
+|Dark 06|titleBar.activeForeground
+|Dark 06|titleBar.inactiveForeground (99)
+
+|Dark 07|editorActiveLineNumber.foreground
+|Dark 07|editorLineNumber.activeForeground
+
+|Dark 08|menu.separatorBackground
+|Dark 08|peekViewResult.lineForeground
+|Dark 08|sideBarTitle.foreground
+
+|Dark 09|editorOverviewRuler.bracketMatchForeground
+|Dark 09|editorOverviewRuler.selectionHighlightForeground (cc)
+|Dark 09|editorOverviewRuler.wordHighlightForeground (cc)
+
+|Dark 10|imagePreview.border (59)
+|Dark 10|panel.border (59)
+|Dark 10|panelTitle.activeBorder (59)
+|Dark 10|sideBarSectionHeader.background (33)
+|Dark 10|terminal.border (59)
+
+|Dark 11|editorOverviewRuler.border (4d)
+|Dark 11|textBlockQuote.background (1a)
+
+|Dark 12|editorOverviewRuler.commonContentForeground (66)
+|Dark 12|merge.commonContentBackground (29)
+|Dark 12|merge.commonHeaderBackground (66)
+
+|Dark 13|editorHoverWidget.border
+|Dark 13|editorSuggestWidget.border
+|Dark 13|editorWidget.border
+|Dark 13|settings.dropdownListBorder
+
+|Dark 14|checkbox.background
+|Dark 14|checkbox.border
+|Dark 14|dropdown.background
+|Dark 14|dropdown.border
+|Dark 14|input.background
+|Dark 14|settings.checkboxBackground
+|Dark 14|settings.checkboxBorder
+|Dark 14|settings.dropdownBackground
+|Dark 14|settings.dropdownBorder
+|Dark 14|titleBar.activeBackground
+|Dark 14|titleBar.inactiveBackground (99)
+
+|Dark 15|editor.findRangeHighlightBackground (66)
+|Dark 15|editor.inactiveSelectionBackground
+
+|Dark 16|activityBar.background
+|Dark 16|debugToolBar.background
+
+|Dark 17|notificationCenterHeader.background
+|Dark 17|notifications.border
+
+|Dark 18|settings.numberInputBackground
+|Dark 18|settings.textInputBackground
+
+|Dark 19|breadcrumbPicker.background
+|Dark 19|editorGroupHeader.tabsBackground
+|Dark 19|editorHoverWidget.background
+|Dark 19|editorSuggestWidget.background
+|Dark 19|editorWidget.background
+|Dark 19|menu.background
+|Dark 19|notifications.background
+|Dark 19|peekViewResult.background
+|Dark 19|quickInput.background
+|Dark 19|sideBar.background
+|Dark 19|tab.border
+
+|Dark 20|breadcrumb.background
+|Dark 20|editor.background
+|Dark 20|editorGroupHeader.noTabsBackground
+|Dark 20|editorGutter.background
+|Dark 20|editorPane.background
+|Dark 20|panel.background
+|Dark 20|peekViewTitle.background
+|Dark 20|tab.activeBackground
+|Dark 20|tab.unfocusedActiveBackground
+|Dark 20|terminal.background
+
+|Dark 21|editorUnnecessaryCode.opacity (aa)
+|Dark 21|listFilterWidget.outline (00)
+|Dark 21|scrollbar.shadow
+|Dark 21|statusBarItem.prominentBackground (80)
+|Dark 21|statusBarItem.prominentHoverBackground (4d)
+|Dark 21|terminal.ansiBlack
+|Dark 21|widget.shadow
+
+|Blue 01|editorInfo.foreground
+|Blue 01|editorMarkerNavigationInfo.background
+|Blue 01|editorOverviewRuler.infoForeground
+
+|Blue 02|editorOverviewRuler.incomingContentForeground (80)
+|Blue 02|merge.incomingContentBackground (33)
+|Blue 02|merge.incomingHeaderBackground (80)
+
+|Blue 03|notificationLink.foreground
+|Blue 03|pickerGroup.foreground
+|Blue 03|textLink.activeForeground
+|Blue 03|textLink.foreground
+
+|Blue 04|editor.hoverHighlightBackground (40)
+|Blue 04|editor.selectionBackground
+
+|Blue 05|tab.activeModifiedBorder
+|Blue 05|tab.inactiveModifiedBorder (80)
+|Blue 05|tab.unfocusedActiveModifiedBorder (80)
+|Blue 05|tab.unfocusedInactiveModifiedBorder (40)
+
+|Blue 06|editorSuggestWidget.highlightForeground
+|Blue 06|list.highlightForeground
+
+|Blue 07|activityBarBadge.background
+|Blue 07|editorOverviewRuler.addedForeground (99)
+|Blue 07|editorOverviewRuler.deletedForeground (99)
+|Blue 07|editorOverviewRuler.modifiedForeground (99)
+|Blue 07|editorOverviewRuler.rangeHighlightForeground (99)
+|Blue 07|extensionBadge.remoteBackground
+|Blue 07|inputOption.activeBorder (00)
+|Blue 07|inputValidation.infoBorder
+|Blue 07|peekView.border
+|Blue 07|statusBar.background
+|Blue 07|textBlockQuote.border (80)
+
+|Blue 08|button.background
+|Blue 08||focusBorder (cc)inputOption.activeBackground (66)
+
+|Blue 09|list.activeSelectionBackground
+|Blue 09|menu.selectionBackground
+
+|Blue 10|editorSuggestWidget.selectedBackground
+|Blue 10|list.focusBackground
+
+|Blue 11|peekViewEditor.background
+|Blue 11|peekViewEditorGutter.background
+
+|Red 01|editorInfo.foreground
+|Red 01|editorLightBulbAutoFix.foreground
+|Red 01|editorMarkerNavigationInfo.background
+|Red 01|editorOverviewRuler.infoForeground
+|Red 01|notificationsInfoIcon.foreground
+
+|Red 02|editorOverviewRuler.incomingContentForeground (80)
+|Red 02|incomingContentBackground (33)
+|Red 02|incomingHeaderBackground (80)
+
+|Red 03|notificationLink.foreground
+|Red 03|pickerGroup.foreground
+|Red 03|textLink.activeForeground
+|Red 03|textLink.foreground
+
+|Red 04|editor.hoverHighlightBackground (40)
+|Red 04|editor.selectionBackground
+|Red 04|minimap.selectionHighlight
+
+|Green 01|gitDecoration.addedResourceForeground
+|Green 01|editor.focusedStackFrameHighlightBackground
+|Green 01|gitDecoration.untrackedResourceForeground
+|Green 01|editorLink.activeForeground
+
+|Green 02|editorOverviewRuler.incomingContentForeground (80)
+|Green 02|merge.incomingContentBackground (33)
+|Green 02|merge.incomingHeaderBackground (80)
+|Green 02|terminal.ansiBrightGreen
+
+|Green 03|statusBarItem.remoteBackground
+|Green 03|editorGutter.addedBackground
+
+|Green 04|extensionButton.prominentBackground
+|Green 04|extensionButton.prominentHoverBackground
+|Green 04|editorBracketMatch.background
+
+|Purple 01|editorOverviewRuler.wordHighlightStrongForeground
+|Purple 01|gitDecoration.conflictingResourceForeground
+
+|Purple 02|terminal.ansiMagenta
+|Purple 02|terminal.ansiBrightMagenta
+
+|Purple 03|statusBar.noFolderBackground
+
+|Yellow 01|gitDecoration.modifiedResourceForeground
+|Yellow 01|textPreformat.foreground
+
+|Yellow 02|editorMarkerNavigationWarning.background
+|Yellow 02|editorOverviewRuler.warningForeground
+|Yellow 02|editorWarning.foreground
+|Yellow 02|list.warningForeground
+
+|Yellow 03|inputValidation.warningBorder
+|Yellow 03|list.invalidItemForeground
+|Yellow 03|inputValidation.warningBackground
+
+|Yellow 04|terminal.ansiYellow
+|Yellow 04|terminal.ansiBrightYellow
+|Yellow 04|editor.stackFrameHighlightBackground
+
+|Orange 01|statusBar.debuggingBackground
+|Orange 01|editorOverviewRuler.findMatchForeground (7e)
+|Orange 01|minimap.findMatchHighlight
+
+|Orange 02|peekViewEditor.matchHighlightBackground
+|Orange 02|editor.findMatchHighlightBackground (55)
+|Orange 02|peekViewResult.matchHighlightBackground (4d)
+
+
+ */
